fcce71b620b72dbf7b8b675c3d9434764338410f
Gaurav Gosain <itsgauravgosain@gmail.com>
2023-08-20 23:36:45 +0400

    fix

--- examples/calculate_tokens/main.go
+++ examples/calculate_tokens/main.go
@@ -8,40 +8,7 @@ import (
 
 func main() {
 	//? Get your API key from https://platform.openai.com/account/api-keys
-	ai := OpenAI.Create("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") // Ideally, you should store your API key in an environment variable
-
-	jsonSchema := OpenAI.Definition{
-		Type: OpenAI.Object,
-		Properties: map[string]OpenAI.Definition{
-			"Instrument": {
-				Type: OpenAI.String,
-				Enum: []string{
-					"EURUSD", "EURAUD", "NZDJPY", "GBPUSD", "GBPJPY", "EURJPY", "USDJPY", "USDCHF", "EURCHF",
-					"AUDUSD", "USDCAD", "NZDUSD", "GBPCHF", "AUDJPY", "AUDCAD", "AUDCHF", "AUDNZD", "CADCHF",
-					"CHFJPY", "EURNZD", "EURCAD", "EURGBP", "EURSEK", "GBPAUD", "GBPCAD", "GBPNZD", "NZDCAD",
-					"NZDCHF", "NZDJPY", "USDSGD", "USDTRY", "USDZAR", "XAUUSD", "XAGUSD",
-				},
-			},
-			"OrderType": {
-				Type: OpenAI.String,
-				Enum: []string{
-					"buy", "Buy", "BUY",
-					"sell", "Sell", "SELL",
-					"buy limit", "Buy Limit", "BUY LIMIT",
-					"sell limit", "Sell Limit", "SELL LIMIT",
-					"buy stop", "Buy Stop", "BUY STOP",
-					"sell stop", "Sell Stop", "SELL STOP",
-				},
-			},
-			"EntryPrice": {Type: OpenAI.Number},
-			"SL":         {Type: OpenAI.Number},
-			"TPs": {
-				Type:  OpenAI.Array,
-				Items: &OpenAI.Definition{Type: OpenAI.Number},
-			},
-		},
-		Required: []string{"Instrument", "OrderType", "EntryPrice", "SL", "TPs"},
-	}
+	ai := OpenAI.Create("YOUR_API_KEY_HERE") // Ideally, you should store your API key in an environment variable
 
 	//? Refer to https://platform.openai.com/docs/api-reference/chat/create to learn more about the parameters.
 	chatRequest := OpenAI.OpenAIChatRequest{
@@ -60,15 +27,6 @@ func main() {
 				Role:    OpenAI.ChatMessageRoleAssistant,
 				Content: "Hello World! This is an example of an assistant response.",
 			},
-		},
-		Functions: []OpenAI.FunctionDefinition{
-			{
-				Name:       "valid_json",
-				Parameters: jsonSchema,
-			},
-		},
-		FunctionCall: OpenAI.FunctionCall{
-			Name: "valid_json",
 		},
 	}
 
